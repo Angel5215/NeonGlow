74376f5ddd592e11da789ae945247cdb5b0e4e22
Ángel Vázquez <angel_betta@outlook.com>
2020-10-26 01:11:54 -0600

    Add output to script to indicate script progress

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Copying themes to folder."
+
 # Create folder if it doesn't exist
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
 # Copy themes to Xcode Font and Color Themes directory
 cp *.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+
+echo "Themes installed successfully"
